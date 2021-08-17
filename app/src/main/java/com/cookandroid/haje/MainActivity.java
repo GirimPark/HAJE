edc43874a7a3b542f9cd5175a6069b02ab708625
girm1999 <zzrlfla@naver.com>
2021-08-17 16:40:53 +0900

    only open kakaonavi

--- app/src/main/java/com/cookandroid/haje/MainActivity.java
+++ app/src/main/java/com/cookandroid/haje/MainActivity.java
@@ -70,12 +70,14 @@ public class MainActivity extends AppCompatActivity {
         if(NaviClient.getInstance().isKakaoNaviInstalled(this)){
             Log.i("카카오내비", "카카오내비 앱으로 길안내 가능");
 
-            // 경유지 없는 경우
-            NaviClient.getInstance().navigateIntent(
-                    new Location("카카오 판교오피스", "127.108640", "37.402111"),
-                    new NaviOption(CoordType.WGS84)
-            );
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.locnall.KimGiSa");
+            startActivity(intent);
 
+            // 경유지 없는 경우
+//            NaviClient.getInstance().navigateIntent(
+//                    new Location("카카오 판교오피스", "127.108640", "37.402111"),
+//                    new NaviOption(CoordType.WGS84)
+//            );
 
             // 경유지 있는 경우
 //            ArrayList<Location> stopover = new ArrayList<Location>();
