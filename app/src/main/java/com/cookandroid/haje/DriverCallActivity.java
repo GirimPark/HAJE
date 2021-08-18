5fe40d83247f300f624df6f227d04c848ed48fa8
pyomin <hpmfool@naver.com>
2021-08-18 20:44:59 +0900

    request_page22

--- app/src/main/java/com/cookandroid/haje/DriverCallActivity.java
+++ app/src/main/java/com/cookandroid/haje/DriverCallActivity.java
@@ -2,7 +2,12 @@ package com.cookandroid.haje;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class DriverCallActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class DriverCallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_call);
+
+
+
+        //호출하기 버튼 눌렀을 때
+        ImageButton callDriverButton = findViewById(R.id.btnNext);
+        callDriverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GpsActivity.class);
+                intent.putExtra("name", "mike");
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
+
