40c752932a0647a9b07d8dd529d1d7c09355acbf
pyomin <hpmfool@naver.com>
2021-08-19 20:57:47 +0900

    rerer

--- app/src/main/java/com/cookandroid/haje/MyPageActivity.java
+++ app/src/main/java/com/cookandroid/haje/MyPageActivity.java
@@ -2,7 +2,10 @@ package com.cookandroid.haje;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MyPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MyPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        ImageButton BtnShowRide =findViewById(R.id.btn_show_ride);
+
+        BtnShowRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ShowRideActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
