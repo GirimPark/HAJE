a4a4350859df7581376b0db7c6c41118642c53e8
girm1999 <zzrlfla@naver.com>
2021-08-20 12:06:36 +0900

    발표1

--- app/src/main/java/com/cookandroid/haje/WebViewActivity.java
+++ app/src/main/java/com/cookandroid/haje/WebViewActivity.java
@@ -43,6 +43,6 @@ public class WebViewActivity extends AppCompatActivity {
             }
         });
 
-        browser.loadUrl("http://192.168.35.135:8081/daum.html");
+        browser.loadUrl("http://1.240.193.92:80/daum.html");
     }
 }
